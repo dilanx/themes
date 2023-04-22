758e01e81d6f927e4ad79553ab509502416aaaf4
Dilan <dilan@dilanxd.com>
2023-04-22 02:14:40 -0500

    update

---
+++ docusaurus-footer-content/index.js
@@ -0,0 +1,46 @@
+module.exports = {
+  content: [
+    {
+      title: 'Community',
+      items: [
+        {
+          label: 'Instagram',
+          href: 'https://www.instagram.com/dilan4k/',
+        },
+        {
+          label: 'GitHub',
+          href: 'https://github.com/dilanx',
+        },
+        {
+          label: 'LinkedIn',
+          href: 'https://www.linkedin.com/in/dilann/',
+        },
+      ],
+    },
+    {
+      title: 'Resources',
+      items: [
+        {
+          label: 'System Status',
+          href: 'https://dilan.statuspage.io/',
+        },
+        {
+          label: 'Sponsor',
+          href: 'https://github.com/sponsors/dilanx',
+        },
+        {
+          label: 'Privacy',
+          href: 'https://www.dilanxd.com/privacy',
+        },
+        {
+          label: 'Attributions',
+          href: 'https://www.dilanxd.com/attributions',
+        },
+        {
+          label: 'Contact Me',
+          href: 'https://www.dilanxd.com/contact',
+        },
+      ],
+    },
+  ],
+};
